8d37446a61cf2723d9ad3c3cd3f0218b38e694a3
lo <lothario.wu@gmail.com>
2021-02-23 17:25:50 +0800

    ok

--- core/init-repo.el
+++ core/init-repo.el
@@ -1,8 +1,8 @@
 ;; pkgs repo
 (require 'package)
-(setq package-archives '(("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-             ("gnu-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
-             ("org-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
+(setq package-archives '(("melpa-cn" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+             ("gnu-cn" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+             ("org-cn" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
 
 (setq package-check-signature nil)
 
