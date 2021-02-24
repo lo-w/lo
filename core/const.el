e138f651e47ee8992c64b3939f4dfadf85419252
lo <lothario.wu@gmail.com>
2021-02-24 17:20:56 +0800

    add some settings

--- core/const.el
+++ core/const.el
@@ -1,6 +1,27 @@
 ;; const
+
+(when (version< emacs-version "25.1")
+  (error "Emacs require version 25.1 or newer, but you're running %s" emacs-version))
+
 (defconst *mac* (eq system-type 'darwin))
 (defconst *linux* (eq system-type 'gnu/linux))
 (defconst *windows* (or (eq system-type 'ms-dos) (eq system-type 'windows-nt)))
+
+(defvar lo-user (getenv (if *windows* "USERNAME" "USER")))
+
+(defvar lo-packages
+  '(ace-window
+    crux
+    easy-kill
+    flycheck
+    git-timemachine
+    gitconfig-mode
+    gitignore-mode
+    projectile
+    magit
+    smartparens
+    undo-tree
+    which-key)
+  "A list of packages to ensure are installed at launch.")
 
 (provide 'const)
