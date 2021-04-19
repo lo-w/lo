3e9b6625e62d093b265d5a91735298d92c764125
ewuxren <renjun.wu@ericsson.com>
2021-04-19 15:22:32 +0800

    add dump bat file

---
+++ dump.bat
@@ -0,0 +1,21 @@
+@echo off
+rem check if the pdmp file exists
+if exist emacs.pdmp (
+    goto clean 
+) else (
+    goto dump
+)
+
+:clean
+echo remove the old pdmp file
+del emacs.pdmp
+goto dump
+
+:dump
+echo dump the emacs
+
+set D_HOME=%cd%
+cd  ../../emacs*/bin
+set E_HOME=%cd%
+
+%E_HOME%\emacs.exe --batch -Q -l %D_HOME%\dump.el
