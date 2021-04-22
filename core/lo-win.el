17d58a4f282ff952749a2770195cec6acb7b7f52
Renjun Wu <renjun.wu@ericsson.com>
2021-04-22 16:12:56 +0800

    change the font size & untrack var dir

--- core/lo-win.el
+++ core/lo-win.el
@@ -9,9 +9,9 @@
   ;; Font settings
   (if *win*
     (progn
-      (set-face-attribute 'default nil :font "Cascadia code 16")
+      (set-face-attribute 'default nil :font "Cascadia code 13")
       (dolist (charset '(kana han symbol cjk-misc bopomofo))
-        (set-fontset-font (frame-parameter nil 'font) charset (font-spec :family "Cascadia code" :size 16))))
+        (set-fontset-font (frame-parameter nil 'font) charset (font-spec :family "Cascadia code" :size 13))))
     (set-face-attribute 'default nil :font "Source Code Pro for Powerline 11")))
 
 (lo-toggle-fullscreen)
